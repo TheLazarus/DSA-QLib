aafd3b2dff349445c83d9202be7b79cac144a7ab
Sarthak <ss22072000@gmail.com>
2024-06-15 00:25:17 +0530

    Fixing some issues with the bipartite imp

--- graphs/isGraphBipartite.js
+++ graphs/isGraphBipartite.js
@@ -1,28 +1,36 @@
 function isGraphBipartite(graph) {
   const nodes = graph.length;
+  let visited = new Array(nodes).fill(-1);
+  const queue = [startingNode];
 
   for (let i = 0; i < graph.length; i++) {
-    const startingNode = i;
-    let visited = new Array(nodes).fill(-1);
-
-    const queue = [startingNode];
-    visited[startingNode] = 0;
-
-    while (queue.length) {
-      let node = queue.shift();
-
-      for (let nhb of graph[node]) {
-        if (visited[nhb] === -1) {
-          visited[nhb] = !visited[node];
-          queue.push(nhb);
-        } else if (visited[nhb] === visited[node]) {
-          return false;
-        }
+    if (visited[i] === -1) {
+      if (check(i, queue, visited, graph) === false) {
+        return false;
       }
     }
   }
 
   return true;
+}
+
+function check(i, queue, visited, graph) {
+  const startingNode = i;
+
+  visited[startingNode] = 0;
+
+  while (queue.length) {
+    let node = queue.shift();
+
+    for (let nhb of graph[node]) {
+      if (visited[nhb] === -1) {
+        visited[nhb] = !visited[node];
+        queue.push(nhb);
+      } else if (visited[nhb] === visited[node]) {
+        return false;
+      }
+    }
+  }
 }
 
 console.log(
